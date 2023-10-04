2abd4ae64de76ddf04716d9095d19ce85a618d8e
larakoseff <larakoseff@gmail.com>
2023-10-04 16:13:09 +0200

    dividing numbers result in a decimal number

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// scripts.js
+
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+try {
+    result.innerText = Math.floor(dividend / divider);
+}
+catch {
+
+}
+
+
+});
