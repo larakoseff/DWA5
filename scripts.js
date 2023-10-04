2dd01bd0168dd1daa84a947486ee6bb0f5a752f0
larakoseff <larakoseff@gmail.com>
2023-10-04 17:33:19 +0200

    Providing anything that is NaN crashes the program

--- scripts.js
+++ scripts.js
@@ -13,4 +13,21 @@ form.addEventListener("submit", (event) => {
       } else {
           result.innerText = Math.floor(dividend / divider);
       }
+      
+    if (dividend > 0 && divider < 0) {
+        result.innerText = "Division not performed. Invalid number provided. Try again.";
+      } else {
+        try {
+          result.innerText = Math.floor(dividend / divider);
+        } catch (e) {
+            console.log("Error:", e)
+        }
+      }  
+      
+      if ( isNaN(dividend) || isNaN(divider)) {
+        document.body.innerHTML = /* html */ `Something critical went wrong. Please reload the page`
+        }
+
     });
+
+    
