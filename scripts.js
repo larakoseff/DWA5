90ca8ecdbf2da7dba62503b692599755a8b98f8b
larakoseff <larakoseff@gmail.com>
2023-10-04 20:59:11 +0200

    resolve console log

--- scripts.js
+++ scripts.js
@@ -7,27 +7,27 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-
+  try {
+    result.innerText = Math.floor(dividend / divider);
     if (dividend === "" || divider === "") {
         result.innerText = "Division not performed. Both values are required in inputs. Try again.";
-      } else {
-          result.innerText = Math.floor(dividend / divider);
-      }
+        throw new Error("Both values are required in inputs.")
+      } 
       
     if (dividend > 0 && divider < 0) {
         result.innerText = "Division not performed. Invalid number provided. Try again.";
-      } else {
-        try {
-          result.innerText = Math.floor(dividend / divider);
-        } catch (e) {
-            console.log("Error:", e)
-        }
-      }  
-      
-      if ( isNaN(dividend) || isNaN(divider)) {
+        throw new Error("Invalid number provided.")
+      } 
+
+    if ( isNaN(dividend) || isNaN(divider)) {
         document.body.innerHTML = /* html */ `Something critical went wrong. Please reload the page`
+        throw new Error("Something critical went wrong")
         }
 
+        } catch(error) {
+            console.log(error)
+        }
+      
     });
 
     
